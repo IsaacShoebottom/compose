5370f63fde551c1a591f7cd787c1bbc9653e9d9e
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-10-18 17:09:39 -0300

    Add invite code script

---
+++ bluesky-pds/create-invite.sh
@@ -0,0 +1,2 @@
+read -p "Enter admin password: " password
+curl -X POST -u admin:$password -H "Content-Type: application/json" -d '{"useCount: 1}' https://shoebottom.ca/xrpc/com.atproto.server.createInviteCode
